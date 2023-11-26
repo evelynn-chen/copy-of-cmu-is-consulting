fcfbb67384d5b3ff38e96201c8918d58a4f27bee
Evelynn Chen <evelynnc@andrew.cmu.edu>
2023-11-26 11:54:15 -0600

    fixed learn more opacity, switched images to given assets

--- index.js
+++ index.js
@@ -2,70 +2,114 @@ const data = [
     {
         title: 'Greater Washington County Food Bank',
         description: "The Greater Washington County Food Bank's mission is to help those who are fighting against hunger. The CMU IS students' role is to help standardize the onboarding process for volunteers and incoming staff. The result was an improved understanding of the food bank's CRM for both staff and volunteers.",
-        logo: './images/logos/GWCFB.png',
+        logo: './assets/SP23-Logos/GWCFB.png',
+        communityPartners: ['Cherise Sandrock'],
+        studentConsultingTeam: ['Emma Kwan', 'Meghana Tera', 'Ricky Lee', 'Yize Shen'],
         execSummary: './exec-summaries/GWCFB.pdf',
-        tags: 'sdg-tech zerohunger userexperience'
+        tags: 'zerohunger userexperience'
     },
     {
         title: 'Filipino-American Association of Pittsburgh',
         description: "The Filipino-American Association of Pittsburgh's mission is to preserve, promote, and perpetuate the Filipino heritage among its members and the general public through cultural, charitable, and educational activities. The CMU IS students improved their data management due to their lack of centralization and automation. The result was an improved website with automated data collection and transfer of legacy data into their new system.",
-        logo: './images/logos/FAAP.jpg',
+        logo: './assets/SP23-Logos/FAAP.jpg',
+        communityPartners: ['Leilani Mears'],
+        studentConsultingTeam: ['Lea De Vylder', 'Michelle Liu', 'Alex Barrios'],
         execSummary: './exec-summaries/FAAP.pdf',
-        tags: 'sdg-tech equality peaceandinclusion databasesolution webapp'
+        tags: 'equality peaceandinclusion databasesolution webapp'
     },
     {
         title: 'The Italian Garden Project',
         description: 'The mission of The Italian Garden Project is to celebrate the joy and wisdom inherent in the traditional Italian American vegetable garden, preserving this heritage and demonstrating its relevance for reconnecting to our food, our families and the earth. The CMU IS students created The Legacy Fig Tree Archive website that allows individuals to enter their fig tree into the archive and search the database for the fig trees. The archive demonstrates the prevalence of the fig growing tradition throughout the US and creates a sense of community for fig tree owners.',
-        logo: './images/logos/IGP.png',
+        logo: '/assets/SP23-Logos/IGP.png',
+        communityPartners: ['Mary Menniti'],
+        studentConsultingTeam: ['Anna Rippert', 'Cami Wu', 'Daniel Arriola'],
         execSummary: './exec-summaries/IGP.pdf',
-        tags: 'sdg-tech lifeonland promotehealth qualityeducation responsibleconsumption zerohunger webapp userexperience databasesolution'
+        tags: 'lifeonland promotehealth qualityeducation responsibleconsumption zerohunger webapp userexperience databasesolution'
     },
     {
         title: 'New Sun Rising',
         description: 'New Sun Rising (NSR) is a nonprofit organization that organizes and supports community development initiatives within Pittsburgh, with the mission "Build vibrant communities through culture, sustainability, and opportunity". The CMU IS students worked with New Sun Rising to create a digital form to collect contextual data (opinions/fears/hopes) of members of a community and store it within their database. This will add value by giving additional information to properly understand problems local Pittsburgh communities are facing.',
-        logo: './images/logos/NewSunRising.png',
+        logo: './assets/SP23-Logos/NewSunRising.png',
+        communityPartners: ['Scott Wolovich'],
+        studentConsultingTeam: ['Mahith Edula', 'Neel Mandapati', 'Zexuan Li', 'Ziyan Wang'],
         execSummary: './exec-summaries/NewSunRising.pdf',
-        tags: 'sdg-tech partnershipsforgoals peaceandinclusion api databasesolution technologystrategy userexperience'
+        tags: 'partnershipsforgoals peaceandinclusion api databasesolution technologystrategy userexperience'
     },
     {
         title: 'Project Theia',
         description: "Project Theia's mission is to provide oculoplastic medical and surgical care to the underserved global community. The CMU IS students furthered this mission by enhancing remote communication with all of their Global Providers, preparing Project Theia to scale. The team utilized Slack to centralize communication, organize educational resources, and support effective remote communication.",
-        logo: './images/logos/ProjectTheia.jpg',
+        logo: './assets/SP23-Logos/ProjectTheia.jpg',
+        communityPartners: ['Jenny Yu'],
+        studentConsultingTeam: ['Brianna Wang', 'Erica Fu', 'Victoria Zhang'],
         execSummary: './exec-summaries/ProjectTheia.pdf',
-        tags: 'sdg-tech goodhealthandwellbeing promotehealth applicationintegration'
+        tags: 'goodhealthandwellbeing promotehealth applicationintegration'
     },
     {
         title: 'Women Against Abusive Relationships',
         description: "Women Against Abusive Relationships (WAAR) is a Pittsburgh-based volunteer organization that aims to break the cycle of domestic violence by providing education, awareness, and prevention programs, directed by Roxanne Epperson. The CMU IS student team revamped the website to meet ADA compliance, and restructured the information architecture to optimize usability, to help users find the resources they need quickly and efficiently.",
-        logo: './images/logos/WAAR.png',
+        logo: './assets/SP23-Logos/WAAR.png',
+        communityPartners: ['Roxanne Epperson'],
+        studentConsultingTeam: ['Amy Deng', 'Cathryn Lin', 'Lucy Li'],
         execSummary: './exec-summaries/WAAR.pdf',
-        tags: 'sdg-tech genderequality promotehealth peaceandinclusion softwareintegration technologystrategy userexperience'
+        tags: 'genderequality promotehealth peaceandinclusion softwareintegration technologystrategy userexperience'
     },
     {
         title: 'Queens Gambit Chess Institute',
         description: "Queens Gambit is an organization that aims to empower youth through chess, especially those who are underrepresented in the chess community. The team implemented two solutions for Queens Gambit: a new online tournament pairing system and updating their community partner relationship management system. As a result, Queens Gambit is able to run their classes and tournaments more effectively and manage their partner data in a more organized manner.",
-        logo: './images/logos/QueensGambit.jpeg',
+        logo: './assets/SP23-Logos/QueensGambit.jpeg',
+        communityPartners: ['Ashley Lynn Priore'],
+        studentConsultingTeam: ["Angela Chen", 'Saaatvik Korisepati', 'Lisa Leung', 'Emmeline Wetzel'],
         execSummary: './exec-summaries/QueensGambit.pdf',
-        tags: 'sdg-tech equality genderequality webapp userexperience softwareintegration'
+        tags: 'equality genderequality webapp userexperience softwareintegration'
     },
     {
         title: 'Operation Better Block/NewEra412',
         description: "NewEra412 is the information management system for the city of Pittsburgh's violence prevention initiatives. The CMU IS student team commissioned the best software development partner for NewEra412, fixed bugs, and designed new features. Now NewEra412 can focus on sustained growth and longevity with a solid vision and the support to back it up.",
-        logo: './images/logos/OBB.png',
+        logo: './assets/SP23-Logos/OBB.png',
+        communityPartners: ['Taili Thompson'],
+        studentConsultingTeam: ['Annie Wu', 'Dora Xiao', 'Rohan Butani'],
         execSummary: './exec-summaries/NewEra412.pdf',
-        tags: 'sdg-tech equality technologystrategy userexperience databasesolution'
+        tags: 'equality technologystrategy userexperience databasesolution'
     },
     {
         title: 'Protect Penn-Trafford',
         description: "Protect Penn-Trafford (Protect PT) is committed to protecting Penn-Trafford and surrounding communities from the harmful environmental effects of energy projects through education and legal advocacy. The CMU IS students developed a web application with functionality to allow data entry and retrieval of information related to energy projects. The web application was used as a working prototype to demonstrate to potential funders the potential of this system.",
-        logo: './images/logos/ProtectPT.jpg',
+        logo: './assets/SP23-Logos/ProtectPT.jpg',
+        communityPartners: ['Chelsea Buell', 'Jim Cirilano', 'Gillian Graber'],
+        studentConsultingTeam: ['Clara Lam', 'Jason Perez', 'Michael Song'],
         execSummary: './exec-summaries/ProtectPT.pdf',
-        tags: 'sdg-tech cleanenergy cleanwater climateaction lifeonland peaceandinclusion promotehealth responsibleconsumption sustainablecities databasesolution webapp'
+        tags: 'cleanenergy cleanwater climateaction lifeonland peaceandinclusion promotehealth responsibleconsumption sustainablecities databasesolution webapp'
+    },
+    {
+        title: 'JADA House International',
+        description: "JADA House International uplifts the Hazelwood and Greater Pittsburgh community by providing an inclusive physical space where teens, senior citizens, and families can build positive relationships within their community and strengthen their social, emotional, and spiritual lives. The CMU IS students developed and redesigned the organizations' current website to enhance community engagement, increase communication, and attract more donors while taking advantage of website analytics to better serve their community. The impact of our project is expected to attract new members and donors while effectively advertising programs and services to serve more individuals in need.",
+        logo: './assets/SP23-Logos/JADA_House.png',
+        communityPartners: ['Terri Shields'],
+        studentConsultingTeam: ['Anesha Santhanam', 'Emily Ngo', 'Fernanda Molina', 'Valeria Cordova'],
+        execSummary: './exec-summaries/JADA_House.pdf',
+        tags: 'peaceandinclusion dataanalytics userexperience webapp'
+    },
+    {
+        title: 'Center for Shared Prosperity',
+        description: 'The Center for Shared Prosperity aims to reduce structural barriers to equitable prosperity in the Pittsburgh region. In order to help achieve this, the CMU IS student team created a website to showcase local minoritized businesses to students and also made a process document that covers how to host a local business on campus. Both of these deliverables are part of an initiative to increase CMU engagement with local businesses, both in terms of awareness and spending',
+        logo: './assets/SP23-Logos/CSP.png',
+        communityPartners: ['Jordan Mrozia'],
+        studentConsultingTeam: ['Pranav Addepalli', 'David Matovic', 'Sean Pham', 'Dillon Shu'],
+        execSummary: './exec-summaries/CSP.pdf',
+        tags: 'cleanenergy climateaction equality genderequality lifeonland partnershipsforgoals peaceandinclusion promotehealth sustainablecities workandeconomicgrowth zerohunger databasesolution searchengineoptimization userexperience webapp'
+    },
+    {
+        title: 'Youth Enrichment Services',
+        description: 'Youth Enrichment Services (YES) aims to empower socially and economically at-risk youth to achieve success through academic preparation, career exploration, and cultural and social leadership development. The CMU IS student team developed a mobile and web application to streamline the arduous SMS communication process with YES students, which was previously time-intensive and constrained staff capacity. With this automated communication system, all YES students can get notified about events in a timely manner while improving YES’ capacity for student enrichment.',
+        logo: './assets/SP23-Logos/YES.png',
+        communityPartners: ['Deanna Sinex'],
+        studentConsultingTeam: ['Colin May', 'Nathan Wai', 'Daniel Gunawan'],
+        execSummary: './exec-summaries/YES.pdf',
+        tags: 'peaceandinclusion promotehealth qualityeducation workandeconomicgrowth webapp api softwareintegration mobileapp'
     }
-
 ]
 
-function createProject(title, description, execSummary, logo) {
+function createProject(title, description, logo, communityPartners, studentConsultingTeam, tags, execSummary) {
     const projectContainer = document.createElement('div');
     projectContainer.className = 'col-lg-2 col-md-4 col-sm-6 my-2';
     const project = document.createElement('div');
@@ -81,17 +125,29 @@ function createProject(title, description, execSummary, logo) {
     learnMore.style.backgroundColor = '#6D6E71';
     learnMore.textContent = 'Learn More';
 
+    project.style.transition = 'transform 0.5s ease';
     project.addEventListener('mouseenter', () => {
         learnMore.classList.remove('d-none');
+        project.style.transform = 'translateY(-7px)';
+        projectLogo.style.opacity = '0.3';
     });
 
     project.addEventListener('mouseleave', () => {
         learnMore.classList.add('d-none');
+        project.style.transform = '';
+        projectLogo.style.opacity = '1';
+
     });
 
     learnMore.addEventListener('click', () => {
+        const communityPartnersContainer = document.getElementById('communityPartners');
+        communityPartnersContainer.innerHTML = `<span class="modal-heading">Community Partner(s)</span>: ${communityPartners.join(', ')}`;
+
+        const studentConsultingTeamContainer = document.getElementById('studentConsultingTeam');
+        studentConsultingTeamContainer.innerHTML = `<span class="modal-heading">Student Consulting Team</span>: ${studentConsultingTeam.join(', ')}`;
+
         const projectDescription = document.getElementById('projectDescription');
-        projectDescription.innerHTML = `<p>${description}</p>`;
+        projectDescription.innerHTML = `<span class="modal-heading">Description</span>: ${description}`;
 
         const modalLogo = document.getElementById('modalLogo');
         modalLogo.src = logo;
@@ -104,6 +160,21 @@ function createProject(title, description, execSummary, logo) {
         execSummaryButton.onclick = function () {
             window.open(execSummary, '_blank');
         };
+
+        const projectTags = document.getElementById('projectTags');
+        projectTags.innerHTML = ''; 
+
+        const badgeContainer = document.createElement('div');
+        badgeContainer.className = 'd-flex flex-wrap';
+
+        tags.split(' ').forEach(tag => {
+            const badge = document.createElement('span');
+            badge.className = 'badge badge-secondary p-2 m-1 carnegie-red font-weight-normal';
+            badge.textContent = tag;
+            badgeContainer.appendChild(badge);
+        });
+
+        projectTags.appendChild(badgeContainer);
 
         $('#projectModal').modal('show');
     });
@@ -121,7 +192,7 @@ function displayProjects(projects) {
     allProjects.className = 'row w-100 mx-auto';
 
     projects.forEach(item => {
-        allProjects.appendChild(createProject(item.title, item.description, item.execSummary, item.logo));
+        allProjects.appendChild(createProject(item.title, item.description, item.logo, item.communityPartners, item.studentConsultingTeam, item.tags, item.execSummary));
     });
 }
 
@@ -132,7 +203,7 @@ function getSelectedCheckboxes(filterId) {
 
 function createFilterBadge(tag, filterId) {
     const badge = document.createElement('span');
-    badge.className = 'badge badge-secondary p-2 m-1 carnegie-red';
+    badge.className = 'badge badge-secondary p-2 m-1 carnegie-red font-weight-normal';
 
     const badgeText = document.createElement('span');
     badgeText.textContent = tag;
@@ -189,7 +260,7 @@ function filterProjects() {
     const filteredProjects = data.filter(project => {
         const hasMatchingSDGTags = selectedSDGs.every(tag => project.tags.includes(tag));
         const hasMatchingTechTags = selectedTech.every(tag => project.tags.includes(tag));
-        const hasMatchingSearch = project.title.toLowerCase().includes(searchTerm);
+        const hasMatchingSearch = project.title.toLowerCase().includes(searchTerm) || project.description.toLowerCase().includes(searchTerm);
 
         return hasMatchingSDGTags && hasMatchingTechTags && hasMatchingSearch;
     });
