982c0fc456c178099aa596005a122e5e0f6470c7
Leo Hsu <leohsu@Leos-MacBook-Pro-8.local>
2023-12-05 20:39:08 -0500

    added bubble

--- index.js
+++ index.js
@@ -3,20 +3,84 @@ var donutColors = ['#C41230','#FDB515','#009647','#008F91','#043673','#007BC0'];
 // bubble chart 
 
 document.addEventListener('DOMContentLoaded', function() {
-    
+    // Bubble chart
+    const bubbleDataValues = [57, 27, 15, 4, 14, 3, 4, 23, 6, 2];
+    const bubbleDataLabels = ['Education', 'Equity', 'Sustainability', 'Business', 'Health', 'Animals', 'Gov.', 'Social Services', 'Food', 'Other'];
+
+    const locationX = [30, 60, 5, 15, 10, 73, 80, 55, 45, 50];
+    const locationY = [150, 235, 80, 45, 190, 180, 175, 100, 270, 220];
+
+
+    const bubbleData = bubbleDataValues.map((value, index) => ({
+        x: locationX[index], // x-coordinate (random for demonstration)
+        y: locationY[index], // y-coordinate (random for demonstration)
+        r: value * 1,  // radius based on data value
+        label: bubbleDataLabels[index], // individual label for each bubble
+    }));
+
+    const bubbleColors = colors.slice(0, bubbleDataValues.length);
+
+    const bubbleCtx = document.getElementById('chBubbleChart').getContext('2d');
+
+    new Chart(bubbleCtx, {
+        type: 'bubble',
+        data: {
+            datasets: bubbleData.map(dataPoint => ({
+                data: [dataPoint],
+                backgroundColor: bubbleColors[bubbleData.indexOf(dataPoint)], // Bubble fill color
+                label: dataPoint.label,
+            })),
+        },
+        options: {
+            scales: {
+                x: {
+                    display: false,
+                    type: 'linear',
+                    position: 'bottom',
+                },
+                y: {
+                    display: false,
+                    type: 'linear',
+                    position: 'left',
+                }
+            },
+            plugins: {
+                datalabels: {
+                    anchor: 'end',
+                    align: 'top',
+                    formatter: function (value, context) {
+                        return context.dataset.label;
+                    },
+                    font: {
+                        weight: 'bold',
+                    },
+                },
+            },
+            tooltips: {
+                callbacks: {
+                    label: function (context) {
+                        const label = context.dataset.label;
+                        const radiusValue = context.parsed.r;
+                        return `${label}: ${radiusValue}`;
+                    },
+                },
+            },
+        },
+    });
+
     // Doughnut chart
     var donutOptions = {
         cutoutPercentage: 85,
-        legend: {position:'bottom', padding:5, labels: {pointStyle:'circle', usePointStyle:true}}
+        legend: { position: 'bottom', padding: 5, labels: { pointStyle: 'circle', usePointStyle: true } }
     };
 
     var donutData = {
-        labels: ['Pittsburgh', 'Remote', 'CMU','Global','National','Pennsylvania'],
+        labels: ['Pittsburgh', 'Remote', 'CMU', 'Global', 'National', 'Pennsylvania'],
         datasets: [
             {
-                backgroundColor: donutColors.slice(0,6),
+                backgroundColor: donutColors.slice(0, 6),
                 borderWidth: 0,
-                data: [118,15,21,2,17,13]
+                data: [118, 15, 21, 2, 17, 13]
             }
         ]
     };
